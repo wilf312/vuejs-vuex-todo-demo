3630ab68db2e363d635e0976922e3fd9f8b26272
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-11-09 13:12:43 +0900

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,32 @@
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    "parser": "babel-eslint",
     sourceType: 'module'
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: [
-    'plugin:vue/recommended',
-    'standard'
+    'plugin:vue/recommended'
   ],
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  'rules': {
+  rules: {
+    // タグの綴じ方
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'never',
+        },
+        svg: 'never',
+        math: 'never',
+      },
+    ],
+
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow debugger during development

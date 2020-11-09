63f9b3653cc896ae97e5768990da972c8339a0ff
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-11-09 16:51:27 +0900

    query を optionsに変更

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,16 +60,16 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
-        query: {
+        loader: 'url-loader',
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
-        query: {
+        loader: 'url-loader',
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
